42ad9307c8401482ace0d2b6b2553ae39422eefc
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2018-05-16 19:58:28 +0200

    Upgrade to Spring Boot 2.x

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /home/gradle/project
 RUN gradle --stacktrace assemble
 
 FROM openjdk:8-jdk-alpine
-COPY --from=0 /home/gradle/project/build/libs/app-0.0.1.jar /app.jar
+COPY --from=0 /home/gradle/project/build/libs/app.jar /app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/kotlin/be/devinnnie/StaticResourceConfig.kt
+++ src/main/kotlin/be/devinnnie/StaticResourceConfig.kt
@@ -3,11 +3,10 @@ package be.devinnnie
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class StaticResourceConfig : WebMvcConfigurerAdapter() {
+class StaticResourceConfig : WebMvcConfigurer {
     @Autowired
     lateinit var worldConfig : WorldConfig
 
